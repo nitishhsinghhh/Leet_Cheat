3fc7a90cd4cc996f2036972fd6b5c945937b4e9c
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-01-06 10:58:47 +0530

    Easy

--- Arrays/C#/4. Median of Two Sorted Arrays.cs
+++ Arrays/C#/4. Median of Two Sorted Arrays.cs
@@ -2,7 +2,7 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {		
         int[] ans = nums1.Concat(nums2).OrderBy(x => x).ToArray();
-		int n = ans.Length;
+	int n = ans.Length;
         decimal medianSorted;
         if (n % 2 !=0) medianSorted = ans[n / 2];
         else medianSorted = Decimal.Divide((ans[n / 2] + ans[(n - 1) / 2]), 2);
@@ -16,11 +16,11 @@ public class Solution {
         int len1=nums1.Length;
         int len2=nums2.Length;
         if (len1<len2) return FindMedianSortedArrays(nums2,nums1);
-		int low=0, high=len2*2;
+	int low=0, high=len2*2;
         while (low<=high) {
-			int mid2=(low+high)/2;
+            int mid2=(low+high)/2;
             int mid1=len1+len2-mid2;
-			double L1=(mid1==0)?Int32.MinValue:nums1[(mid1-1)/2];
+	    double L1=(mid1==0)?Int32.MinValue:nums1[(mid1-1)/2];
             double L2=(mid2==0)?Int32.MinValue:nums2[(mid2-1)/2];
             double R1=(mid1==len1*2)?Int32.MaxValue:nums1[(mid1)/2];
             double R2=(mid2==len2*2)?Int32.MaxValue:nums2[(mid2)/2];
