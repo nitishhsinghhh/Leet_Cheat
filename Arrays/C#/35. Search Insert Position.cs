8747718ad3008482b5154322914a0d4b6e5d092a
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-01-11 22:46:32 +0530

    Easy
    35. Search Insert Position
    
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    You must write an algorithm with O(log n) runtime complexity.
    
    Example 1:
    Input: nums = [1,3,5,6], target = 5
    Output: 2
    
    Example 2:
    Input: nums = [1,3,5,6], target = 2
    Output: 1
    
    Example 3:
    Input: nums = [1,3,5,6], target = 7
    Output: 4
     
    Constraints:
    1 <= nums.length <= 10^4
    -10^4 <= nums[i] <= 10^4
    nums contain distinct values sorted in ascending order.
    -10^4 <= target <= 10^4

---
+++ Arrays/C#/35. Search Insert Position.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int low = 0, high = nums.Length-1;
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            if (nums[mid] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return low;
+    }
+}
