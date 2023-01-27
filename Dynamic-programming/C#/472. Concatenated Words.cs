274f13e5ef0ca01cd63fa69a405783e1a0f0b4fe
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-01-27 18:16:43 +0530

    Hard

---
+++ Dynamic-programming/C#/472. Concatenated Words.cs
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/concatenated-words/
+
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    public class Solution {
+        public IList<string> FindAllConcatenatedWordsInADict(string[] words) {
+            List<string> res = new List<string>();
+            if (words == null || words.Length == 0) return res;
+            HashSet<string> wordSet = new HashSet<string>();
+            Array.Sort(words, (a, b) => a.Length - b.Length);
+            foreach (var word in words) {
+                if (IsConcatenated(word, wordSet)) res.Add(word);
+                wordSet.Add(word);
+            }
+            return res;
+        }
+        public bool IsConcatenated(string word, HashSet<string> wordSet) {
+            if (wordSet.Count == 0) return false;
+            bool[] dp = new bool[word.Length + 1];
+            dp[0] = true;
+            for (int i = 1; i <= word.Length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && wordSet.Contains(word.Substring(j, i - j))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[word.Length];
+        }
+        static void Main(string[] args) {
+            string[] words = {"cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat" };
+            Solution oSolution = new Solution();
+            IList<string> AllWords = oSolution.FindAllConcatenatedWordsInADict(words);
+            foreach (var item in AllWords) {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
